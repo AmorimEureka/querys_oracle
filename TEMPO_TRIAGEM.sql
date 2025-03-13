d15c6d204be0d4c1233270908a6600c18938486d
rafael.amorim <raffaekk@gmail.com>
2025-03-13 20:11:45 -0300

    refact:Avaliando query do painel HPC-CLINICA-Tempo Médio de Atendimento

--- TEMPO_TRIAGEM.sql
+++ TEMPO_TRIAGEM.sql
@@ -18,8 +18,8 @@ FROM
 		sacr_tempo_processo.cd_triagem_atendimento
 	HAVING
 		Count(*) = 1) ;
-		
-	
+
+
 -- Cadastramento de pre-atendimento para classificacao de risco
 SELECT * FROM triagem_atendimento WHERE DS_SENHA = 'UR0012';
 
@@ -31,7 +31,7 @@ SELECT * FROM FILA_SENHA ;
 SELECT * FROM sacr_tempo_processo ;
 
 
-SELECT DISTINCT 
+SELECT DISTINCT
 	fs.DS_FILA ,
 	ta.CD_ATENDIMENTO ,
 --	stp.dh_processo ,
@@ -54,27 +54,27 @@ SELECT DISTINCT
 	stp.CD_MOTIVO_CANCELAMENTO
 FROM sacr_tempo_processo stp
 LEFT JOIN TRIAGEM_ATENDIMENTO ta              ON stp.CD_TRIAGEM_ATENDIMENTO = ta.CD_TRIAGEM_ATENDIMENTO
-LEFT JOIN FILA_SENHA fs 		              ON  ta.CD_FILA_SENHA          = fs.CD_FILA_SENHA 
-LEFT JOIN PRESTADOR p 						  ON  ta.CD_PRESTADOR 			= p.CD_PRESTADOR 
+LEFT JOIN FILA_SENHA fs 		              ON  ta.CD_FILA_SENHA          = fs.CD_FILA_SENHA
+LEFT JOIN PRESTADOR p 						  ON  ta.CD_PRESTADOR 			= p.CD_PRESTADOR
 LEFT JOIN
-		(	SELECT 
+		(	SELECT
 				scr.CD_TRIAGEM_ATENDIMENTO,
-				scr.NM_COR 
+				scr.NM_COR
 			FROM SACR_CLASSIFICACAO_RISCO scr
-			LEFT JOIN SACR_CLASSIFICACAO  sc  ON scr.CD_CLASSIFICACAO      = sc.CD_CLASSIFICACAO 
-			LEFT JOIN SACR_COR_REFERENCIA scr ON sc.CD_COR_REFERENCIA      = scr.CD_COR_REFERENCIA 
+			LEFT JOIN SACR_CLASSIFICACAO  sc  ON scr.CD_CLASSIFICACAO      = sc.CD_CLASSIFICACAO
+			LEFT JOIN SACR_COR_REFERENCIA scr ON sc.CD_COR_REFERENCIA      = scr.CD_COR_REFERENCIA
 ) cor   						              ON ta.CD_TRIAGEM_ATENDIMENTO = cor.CD_TRIAGEM_ATENDIMENTO
 LEFT JOIN
-        (	SELECT 
+        (	SELECT
 				a.CD_ATENDIMENTO ,
 				e.DS_ESPECIALID ,
 				p2.nm_prestador
-			FROM DBAMV.ATENDIME a 
-			LEFT JOIN DBAMV.SERVICO s   	  ON a.CD_SERVICO    = s.CD_SERVICO 
+			FROM DBAMV.ATENDIME a
+			LEFT JOIN DBAMV.SERVICO s   	  ON a.CD_SERVICO    = s.CD_SERVICO
 			LEFT JOIN DBAMV.ESPECIALID e 	  ON s.CD_ESPECIALID = e.CD_ESPECIALID
 			LEFT JOIN DBAMV.PRESTADOR p2      ON a.cd_prestador  = p2.CD_PRESTADOR
-) esp   						              ON ta.CD_ATENDIMENTO = esp.CD_ATENDIMENTO 
-WHERE fs.CD_FILA_SENHA = 1 AND ta.DH_PRE_ATENDIMENTO > TRUNC(ADD_MONTHS(SYSDATE, -3), 'MM') ; 
+) esp   						              ON ta.CD_ATENDIMENTO = esp.CD_ATENDIMENTO
+WHERE fs.CD_FILA_SENHA = 1 AND ta.DH_PRE_ATENDIMENTO > TRUNC(ADD_MONTHS(SYSDATE, -3), 'MM') ;
 
 --AND ta.ds_senha = 'UR0012';
 
@@ -82,9 +82,29 @@ WHERE fs.CD_FILA_SENHA = 1 AND ta.DH_PRE_ATENDIMENTO > TRUNC(ADD_MONTHS(SYSDATE,
 
 
 
-
-
-
+-- Query 'Tempo de Processo' do Painel HPC-CLINICA-Tempo Médio de Atendimento
+SELECT
+  stp.cd_triagem_atendimento,
+  stp.cd_atendimento,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'CADASTRO NO TOTEM' THEN stp.dh_processo END) AS cad_totem,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'ATENDIMENTO ADMINISTRATIVO CHAMADA' THEN stp.dh_processo END) AS adm_chamada,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'ATENDIMENTO ADMINISTRATIVO INÍCIO' THEN stp.dh_processo END) AS adm_início,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'ATENDIMENTO ADMINISTRATIVO FINAL' THEN stp.dh_processo END) AS adm_final,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'ATENDIMENTO MÉDICO CHAMADA' THEN stp.dh_processo END) AS méd_chamada,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'ATENDIMENTO MÉDICO INÍCIO' THEN stp.dh_processo END) AS méd_início,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'ATENDIMENTO MÉDICO ALTA' THEN stp.dh_processo END) AS méd_alta,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'ATENDIMENTO MÉDICO FINAL' THEN stp.dh_processo END) AS méd_final,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'EXAME IMAGEM CHAMADA' THEN stp.dh_processo END) AS img_chamada,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'EXAME DE IMAGEM INÍCIO' THEN stp.dh_processo END) AS img_início,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'EXAME DE IMAGEM FINAL' THEN stp.dh_processo END) AS img_final,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'EXAME LABORATORIAL INÍCIO' THEN stp.dh_processo END) AS lab_início,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'EXAME LABORATORIAL CHAMADA' THEN stp.dh_processo END) AS lab_chamada,
+  MAX(CASE WHEN sttp.ds_tipo_tempo_processo = 'EXAME LABORATORIAL FINAL' THEN stp.dh_processo END) AS lab_final
+FROM sacr_tempo_processo stp
+LEFT JOIN sacr_tipo_tempo_processo sttp ON stp.cd_tipo_tempo_processo = sttp.cd_tipo_tempo_processo
+WHERE stp.cd_atendimento IS NOT NULL
+AND stp.cd_triagem_atendimento IS NOT NULL
+GROUP BY stp.cd_triagem_atendimento, stp.cd_atendimento ;
 
 
 
