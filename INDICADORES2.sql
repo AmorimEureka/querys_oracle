132c21bb688027b3520dc0fa0ab38c2b704ffa09
rafael.amorim <raffaekk@gmail.com>
2025-03-25 14:33:34 -0300

    Refact:Removendo partes desnecessarias do script

--- INDICADORES2.sql
+++ INDICADORES2.sql
@@ -366,6 +366,176 @@ SELECT 'TOTAL GERAL' AS TP_ATENDIMENTO, SUM(CLINICA1), SUM(CLINICA2) FROM PIVOT_
 
 
 
+-- QUERY PRESCRIÇÃO DE EXAMES
+	SELECT
+		a.CD_ATENDIMENTO , --
+		a.TP_ATENDIMENTO ,
+		a.HR_ATENDIMENTO ,
+		a.CD_ORI_ATE ,
+		oa.DS_ORI_ATE ,
+		c.NM_CONVENIO ,
+		pm.CD_PRE_MED ,    --
+		pm.DH_CRIACAO ,
+		tp.CD_TIP_ESQ ,
+		CASE
+			WHEN tp.CD_TIP_ESQ = 'EXI' THEN er.DS_EXA_RX
+			WHEN tp.CD_TIP_ESQ = 'EXL' THEN el.NM_EXA_LAB
+		END AS EXAMES
+	FROM ATENDIME a
+	LEFT JOIN PRE_MED pm
+		ON pm.CD_ATENDIMENTO = a.CD_ATENDIMENTO
+	LEFT JOIN ITPRE_MED im
+		ON pm.CD_PRE_MED = im.CD_PRE_MED
+	LEFT JOIN TIP_PRESC tp
+		ON im.CD_TIP_PRESC = tp.CD_TIP_PRESC
+	LEFT JOIN EXA_RX er
+		ON tp.CD_EXA_RX = er.CD_EXA_RX
+	LEFT JOIN EXA_LAB el
+		ON tp.CD_EXA_LAB = el.CD_EXA_LAB
+	LEFT JOIN ORI_ATE oa
+		ON a.CD_ORI_ATE = oa.CD_ORI_ATE
+	LEFT JOIN CONVENIO c
+		ON a.CD_CONVENIO = c.CD_CONVENIO
+	WHERE tp.CD_TIP_ESQ IN('EXI', 'EXL') AND NOT EXISTS ( SELECT 1 FROM DBAMV.IT_AGENDA_CENTRAL iac WHERE a.cd_atendimento = iac.cd_atendimento ) AND a.TP_ATENDIMENTO IN('A','U','I')
+	;
+
+
+-- QUERY PEDIDOS EXAMES
+/* ADAPTADA PARA RESPONDER AS METRICAS DE IMPLANTACAO UP FLUX */
+WITH PEDIDOS_UP_FLUX
+	AS(
+		SELECT
+			a.CD_ATENDIMENTO ,
+			a.TP_ATENDIMENTO ,
+			a.DT_ATENDIMENTO ,
+			EXTRACT(MONTH FROM a.DT_ATENDIMENTO) AS MES,
+			EXTRACT(YEAR FROM a.DT_ATENDIMENTO) AS ANO,
+			pr.HR_PEDIDO AS DT_PEDIDO ,
+			a.CD_ORI_ATE ,
+			oa.DS_ORI_ATE ,
+			c.NM_CONVENIO ,
+			pm.CD_PRE_MED ,    --
+			pm.DH_CRIACAO ,
+			CAST('EXI' AS VARCHAR2(3)) AS CD_TIP_ESQ ,
+			er.DS_EXA_RX EXAMES
+		FROM  ITPED_RX ir
+		LEFT JOIN PED_RX pr
+			ON ir.cd_ped_rx = pr.cd_ped_rx
+		LEFT JOIN ATENDIME a
+			ON pr.cd_atendimento = a.cd_atendimento
+		LEFT JOIN EXA_RX er
+			ON ir.cd_exa_rx = er.cd_exa_rx
+		LEFT JOIN PRE_MED pm
+			ON pr.CD_PRE_MED = pm.CD_PRE_MED
+		LEFT JOIN ORI_ATE oa
+			ON a.CD_ORI_ATE = oa.CD_ORI_ATE
+		LEFT JOIN CONVENIO c
+			ON a.CD_CONVENIO = c.CD_CONVENIO
+		WHERE a.TP_ATENDIMENTO = 'I'
+		UNION ALL
+		SELECT
+			a.CD_ATENDIMENTO ,
+			a.TP_ATENDIMENTO ,
+			a.DT_ATENDIMENTO ,
+			EXTRACT(MONTH FROM a.DT_ATENDIMENTO) AS MES,
+			EXTRACT(YEAR FROM a.DT_ATENDIMENTO) AS ANO,
+			pl.HR_PED_LAB AS DT_PEDIDO ,
+			a.CD_ORI_ATE ,
+			oa.DS_ORI_ATE ,
+			c.NM_CONVENIO ,
+			pm.CD_PRE_MED ,    --
+			pm.DH_CRIACAO ,
+			CAST('EXL' AS VARCHAR2(3)) AS CD_TIP_ESQ ,
+			el.NM_EXA_LAB EXAMES
+		FROM  ITPED_lab il
+		LEFT JOIN PED_lab pl
+			ON il.cd_ped_lab = pl.cd_ped_lab
+		LEFT JOIN ATENDIME a
+			ON pl.cd_atendimento = a.cd_atendimento
+		LEFT JOIN EXA_LAB el
+			ON il.cd_exa_lab = el.cd_exa_lab
+		LEFT JOIN PRE_MED pm
+			ON pl.CD_PRE_MED = pm.CD_PRE_MED
+		LEFT JOIN ORI_ATE oa
+			ON a.CD_ORI_ATE = oa.CD_ORI_ATE
+		LEFT JOIN CONVENIO c
+			ON a.CD_CONVENIO = c.CD_CONVENIO
+		WHERE a.TP_ATENDIMENTO = 'I'
+),
+FILTROS
+	AS (
+		SELECT
+			ANO || '-' || MES AS ANO_MES,
+			CASE WHEN CD_TIP_ESQ = 'EXI' THEN
+				'EXAME IMAGEM'
+				WHEN CD_TIP_ESQ = 'EXL' THEN
+				'EXAME LABORATORIO'
+			END AS EXAME,
+			COUNT(DISTINCT CD_ATENDIMENTO) AS QTD_ATENDIMENTO
+		FROM PEDIDOS_UP_FLUX
+		WHERE ((ANO = 2024 AND MES = 12) OR (ANO = 2025 AND MES IN (1, 2)))
+		GROUP BY ANO || '-' || MES, CASE WHEN CD_TIP_ESQ = 'EXI' THEN 'EXAME IMAGEM' WHEN CD_TIP_ESQ = 'EXL' THEN 'EXAME LABORATORIO' END
+		ORDER BY ANO || '-' || MES ASC
+),
+PIVOT_TABLE
+	AS (
+		SELECT * FROM FILTROS
+		PIVOT (
+			SUM(QTD_ATENDIMENTO)
+			FOR EXAME IN('EXAME IMAGEM' AS "EXAME_IMAGEM", 'EXAME LABORATORIO' AS "EXAME_LABORATORIO")
+		)
+)
+SELECT ANO_MES, EXAME_IMAGEM, EXAME_LABORATORIO FROM PIVOT_TABLE
+
+UNION ALL
+
+SELECT
+	'TOTAL GERAL' AS ANO_MES,
+	SUM(EXAME_IMAGEM) AS EXAME_IMAGEM,
+	SUM(EXAME_LABORATORIO) AS EXAME_LABORATORIO
+FROM PIVOT_TABLE
+;
+
+WITH ALTA_HOSPITALAR
+	AS (
+		SELECT
+			CD_ATENDIMENTO ,
+			CD_PACIENTE ,
+			CD_CONVENIO ,
+			CD_PRESTADOR ,
+			CD_LEITO ,
+			CD_MOT_ALT ,
+			DT_ATENDIMENTO ,
+			DH_ALTA_LANCADA ,
+			EXTRACT(MONTH FROM DH_ALTA_LANCADA) AS MES,
+			EXTRACT(YEAR FROM DH_ALTA_LANCADA) AS ANO,
+			DT_ALTA ,
+			NM_USUARIO_ALTA ,
+			DT_ALTA_MEDICA ,
+			NM_USUARIO_ALTA_MEDICA ,
+			DT_PREVISTA_ALTA ,
+			TP_ATENDIMENTO
+			FROM ATENDIME WHERE TP_ATENDIMENTO = 'I' AND HR_ALTA IS NOT NULL
+),
+FILTROS
+	AS (
+		SELECT
+			ANO || '-' || MES AS ANO_MES,
+			COUNT(DISTINCT CD_ATENDIMENTO) AS QTD_ATENDIMENTO
+		FROM ALTA_HOSPITALAR
+		WHERE ((ANO = 2024 AND MES = 12) OR (ANO = 2025 AND MES IN (1, 2)))
+		GROUP BY ANO || '-' || MES
+		ORDER BY ANO || '-' || MES ASC
+)
+SELECT ANO_MES, QTD_ATENDIMENTO FROM FILTROS
+
+UNION ALL
+
+SELECT
+	'TOTAL GERAL' AS ANO_MES,
+	SUM(QTD_ATENDIMENTO) AS QTD_ATENDIMENTO
+FROM FILTROS
+;
 
 
 
@@ -373,10 +543,22 @@ SELECT 'TOTAL GERAL' AS TP_ATENDIMENTO, SUM(CLINICA1), SUM(CLINICA2) FROM PIVOT_
 
 
 
-SELECT * FROM DBAMV.SETOR st WHERE st.cd_setor IN (117, 137) ;
+
+
+SELECT * FROM user_tab_columns WHERE TABLE_NAME = 'ATENDIME' AND COLUMN_NAME LIKE '%ALTA%'
+;
+
+SELECT * FROM user_tab_columns WHERE TABLE_NAME = 'PED_LAB'
+;
+
+SELECT * FROM user_tab_columns WHERE TABLE_NAME = 'ITPED_LAB'
+;
 
 
 
+/* ************************************************************************************************ */
+
+-- QUERY FINAL COM OS EXAMES
 WITH CONSULTA_FINAL AS(
 SELECT -- ATENDIDOS AGENDADOS
    agen.cd_agenda_central ,
@@ -793,169 +975,6 @@ WHERE ANO = 2025 AND MES = 1 AND tp_atendimento IN('Consulta', 'Exame')
 GROUP BY STATUS_ATENDIMENTO;
 
 SELECT * FROM CONSULTA_FINAL WHERE EXAMES LIKE '%TESTE ERGOMETRI%' ;
-
-
-
--- *****************************
-WITH CONSULTA_FINAL AS(
-		SELECT -- AGENDADOS *REST [ ABSENTEISTA | OCIOSO ]
-		   i.cd_agenda_central,
-		   DECODE(a.TP_AGENDA,'L','LABORATORIO','I', 'IMAGEM','A', 'AMBULATORIO' ) AS TIPO_AGENDA,
-		   CAST('1' AS NUMBER(8,0)) AS ACRES,
-		   i.hr_agenda,
-		   EXTRACT(MONTH FROM i.hr_agenda) AS MES,
-		   EXTRACT(YEAR FROM i.hr_agenda) AS ANO,
-		   i.cd_atendimento,
-		   i.cd_paciente,
-		   i.nm_paciente,
-		   p.dt_cadastro,
-		   i.dt_nascimento,
-		   i.cd_convenio,
-		   c.nm_convenio,
-		   i.cd_item_agendamento,
-		   ia.ds_item_agendamento,
-		   CASE
-		   		WHEN i.cd_atendimento IS NULL AND i.nm_paciente IS NOT NULL THEN
-		   			'AGENDADO - ABSENTEISMO'
-		   		WHEN i.cd_atendimento IS NOT NULL THEN
-		   			'AGENDADO ATENDIDO'
-		   		ELSE 'ESCALA - OCIOSA'
-		   END AS STATUS_ATENDIMENTO,
-		   i.dt_gravacao,
-		   i.ds_observacao_geral,
-		   i.dh_presenca_falta,
-		   a.cd_recurso_central,
-		   r.ds_recurso_central,
-		   a.cd_prestador,
-		   pr.nm_prestador,
-		   s.cd_setor,
-		   s.nm_setor,
-		    CASE
-		        WHEN i.hr_agenda >= TO_TIMESTAMP('2024-05-27 00:00:00', 'YYYY-MM-DD HH24:MI:SS')
-		             AND s.cd_setor NOT IN (117, 137)
-		             AND a.cd_prestador IN ('245', '268', '277', '318', '366', '372', '619', '645', '655', '682', '747', '762', '787', '874', '925', '945', '960') THEN
-		            'CLINICA 2'
-		        WHEN i.hr_agenda >= TO_TIMESTAMP('2024-05-27 00:00:00', 'YYYY-MM-DD HH24:MI:SS')
-		             AND s.cd_setor NOT IN (117, 137)
-		             AND s.nm_setor LIKE '%2%'
-		             AND s.nm_setor NOT IN ('POSTO 2', 'UTI 2') THEN
-		            'CLINICA 2'
-		        WHEN i.hr_agenda >= TO_TIMESTAMP('2024-05-27 00:00:00', 'YYYY-MM-DD HH24:MI:SS')
-		        	 AND s.cd_setor IN (117, 137) THEN
-		   			'CLINICA 2'
-		        ELSE
-		            'CLINICA 1'
-		    END AS CLINICAS,
-		   a.qt_atendimento,
-		   a.qt_marcados,
-		   i.cd_usuario,
-		   u.nm_usuario,
-		   i.cd_tip_mar,
-		   tr.ds_tip_mar,
-		   CASE
-		   		WHEN el.nm_exa_lab IS NOT NULL THEN 'EXL'
-		   		WHEN er.ds_exa_rx IS NOT NULL THEN 'EXI'
-		   END AS TIPO_EXAMES,
-		   NVL(el.nm_exa_lab, er.ds_exa_rx) AS EXAMES ,
-		   CASE a.tp_agenda
-		   		WHEN 'A' THEN 'Consulta'
-		   		WHEN 'L' THEN 'Exame'
-		   		WHEN 'I' THEN 'Exame'
-		   		ELSE 'Sem Correspondência'
-		   END AS tp_atendimento
-		FROM DBAMV.IT_AGENDA_CENTRAL i
-		LEFT JOIN DBAMV.AGENDA_CENTRAL a    	   ON i.cd_agenda_central   = a.cd_agenda_central
-		LEFT JOIN DBAMV.ITEM_AGENDAMENTO ia 	   ON i.cd_item_agendamento = ia.cd_item_agendamento
-		LEFT JOIN DBAMV.SETOR s             	   ON s.cd_setor            = a.cd_setor
-		LEFT JOIN DBAMV.PRESTADOR pr        	   ON pr.cd_prestador       = a.cd_prestador
-		LEFT JOIN DBAMV.convenio c          	   ON c.cd_convenio         = i.cd_convenio
-		LEFT JOIN DBASGU.USUARIOS u         	   ON u.cd_usuario          = i.cd_usuario
-		LEFT JOIN DBAMV.RECURSO_CENTRAL r   	   ON r.cd_recurso_central  = a.cd_recurso_central
-		LEFT JOIN DBAMV.TIP_MAR tr          	   ON tr.cd_tip_mar         = i.cd_tip_mar
-		LEFT JOIN DBAMV.EXA_LAB el 				   ON ia.CD_EXA_LAB 		= el.CD_EXA_LAB
-		LEFT JOIN DBAMV.EXA_RX er 				   ON ia.CD_EXA_RX          = er.CD_EXA_RX
-		LEFT JOIN DBAMV.PACIENTE p 				   ON i.cd_paciente			= p.cd_paciente
-		WHERE i.cd_atendimento  IS NULL AND	i.cd_it_agenda_pai IS NULL
-)
-SELECT COUNT(*) FROM CONSULTA_FINAL
-WHERE ANO = 2024 AND MES = 8 AND
-	  tp_atendimento = 'Consulta' AND
-	  STATUS_ATENDIMENTO = 'AGENDADO - ABSENTEISMO' AND
-	  DH_PRESENCA_FALTA IS NULL AND (EXTRACT(MONTH FROM dt_cadastro) <> 1 AND EXTRACT(YEAR FROM dt_cadastro) <> 2025) ;
-
-
-
-
-
-WITH MEU_PAU AS(
-SELECT
-		   CASE
-		   		WHEN i.cd_atendimento IS NULL AND i.nm_paciente IS NOT NULL THEN
-		   			'AGENDADO - ABSENTEISMO'
-		   		WHEN i.cd_atendimento IS NOT NULL THEN
-		   			'AGENDADO ATENDIDO'
-		   		ELSE 'ESCALA - OCIOSA'
-		   END AS STATUS_ATENDIMENTO,
-		   CASE a.tp_agenda
-		   		WHEN 'A' THEN 'Consulta'
-		   		WHEN 'L' THEN 'Exame'
-		   		WHEN 'I' THEN 'Exame'
-		   		ELSE 'Sem Correspondência'
-		   END AS tp_atendimento,
-		   p.dt_cadastro,
-		   i.*
-FROM DBAMV.IT_AGENDA_CENTRAL i
-LEFT JOIN DBAMV.AGENDA_CENTRAL a    	   ON i.cd_agenda_central   = a.cd_agenda_central
-LEFT JOIN DBAMV.ITEM_AGENDAMENTO ia 	   ON i.cd_item_agendamento = ia.cd_item_agendamento
-LEFT JOIN DBAMV.PACIENTE p 				   ON i.cd_paciente			= p.cd_paciente
-WHERE i.cd_atendimento  IS NULL AND	i.cd_it_agenda_pai IS NULL
-),
-MEU_PAU2 AS (
-SELECT * FROM MEU_PAU
-WHERE EXTRACT(YEAR FROM hr_agenda)=2024 AND EXTRACT(MONTH FROM hr_agenda)=8
-AND STATUS_ATENDIMENTO= 'AGENDADO - ABSENTEISMO' AND tp_atendimento = 'Consulta'
---AND ( EXTRACT(MONTH FROM dt_cadastro) = 8 AND EXTRACT(YEAR FROM dt_cadastro) = 2024 )
-AND cd_paciente IN (
-		SELECT
-			a.cd_paciente
-		FROM DBAMV.ATENDIME a
-		LEFT JOIN DBAMV.PACIENTE pa                ON a.cd_paciente         = pa.cd_paciente
-		LEFT JOIN DBAMV.PRESTADOR p                ON a.cd_prestador        = p.CD_PRESTADOR
-		LEFT JOIN DBAMV.CONVENIO c                 ON a.cd_convenio         = c.cd_convenio
-		LEFT JOIN DBAMV.ORI_ATE s                  ON a.cd_ori_ate          = s.cd_ori_ate
-		LEFT JOIN DBAMV.TIP_MAR t                  ON a.cd_tip_mar          = t.cd_tip_mar
-		LEFT JOIN DBAMV.ESPECIALID ia              ON a.cd_especialid       = ia.cd_especialid
-		LEFT JOIN DBAMV.SETOR st				   ON s.CD_SETOR            = st.CD_SETOR
-		WHERE EXTRACT(MONTH FROM a.dt_atendimento)=8 AND EXTRACT(YEAR FROM a.dt_atendimento)=2024 AND NOT EXISTS ( SELECT 1 FROM DBAMV.IT_AGENDA_CENTRAL iac WHERE a.cd_atendimento = iac.cd_atendimento ) AND a.tp_atendimento = 'A'
-							)
-)
-SELECT
-COUNT(*)
-FROM MEU_PAU2
-;
-
-
-
-
-
-
-SELECT DISTINCT
-   COUNT(*)
-FROM DBAMV.IT_AGENDA_CENTRAL i
-LEFT JOIN DBAMV.AGENDA_CENTRAL a    	   ON i.cd_agenda_central   = a.cd_agenda_central
-LEFT JOIN DBAMV.ITEM_AGENDAMENTO ia 	   ON i.cd_item_agendamento = ia.cd_item_agendamento
-LEFT JOIN DBAMV.SETOR s             	   ON s.cd_setor            = a.cd_setor
-LEFT JOIN DBAMV.PRESTADOR pr        	   ON pr.cd_prestador       = a.cd_prestador
-LEFT JOIN DBAMV.convenio c          	   ON c.cd_convenio         = i.cd_convenio
-LEFT JOIN DBASGU.USUARIOS u         	   ON u.cd_usuario          = i.cd_usuario
-LEFT JOIN DBAMV.RECURSO_CENTRAL r   	   ON r.cd_recurso_central  = a.cd_recurso_central
-LEFT JOIN DBAMV.TIP_MAR tr          	   ON tr.cd_tip_mar         = i.cd_tip_mar
-WHERE
-	TO_CHAR(i.hr_agenda, 'MM/YYYY')='08/2024' AND
-	i.cd_atendimento  IS NULL
-/*	i.cd_it_agenda_pai IS NULL AND
-	EXISTS ( SELECT 1 FROM DBAMV.ATENDIME ate WHERE ate.cd_atendimento = i.cd_atendimento ) */
-;
 
 
 
