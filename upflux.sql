541f3d55a9b25f84770f7a4a5924db63c20a32c4
rafael.amorim <raffaekk@gmail.com>
2025-04-15 07:38:32 -0300

    upflux

--- upflux.sql
+++ upflux.sql
@@ -71,3 +71,66 @@ GRANT SELECT ON dbasgu.usuarios                 TO upflux;
 -- 4. Conceder permissão de EXECUTE apenas nas funções especificadas
 GRANT EXECUTE ON dbamv.fnc_mv_recupera_data_hora TO upflux;
 GRANT EXECUTE ON dbamv.verif_vl_custo_medio     TO upflux;
+
+
+
+
+grant execute on dbamv.fnc_ffcv_sn_opme 		 to upflux;
+grant execute on dbamv.fnc_sacr_abrevia_nome_pac to upflux;
+grant execute on dbamv.pkg_mv2000 				 to upflux;
+grant execute on dbamv.verif_ds_unid_prod 		 to upflux;
+grant execute on dbamv.verif_vl_custo_medio 	 to upflux;
+grant execute on dbamv.verif_vl_fator_prod  	 to upflux;
+
+grant select on dbamv.age_cir				to upflux;
+grant select on dbamv.atendime              to upflux;
+grant select on dbamv.aviso_cirurgia        to upflux;
+grant select on dbamv.cen_cir               to upflux;
+grant select on dbamv.cid                   to upflux;
+grant select on dbamv.cirurgia              to upflux;
+grant select on dbamv.cirurgia_aviso        to upflux;
+grant select on dbamv.classe                to upflux;
+grant select on dbamv.classifica_asa        to upflux;
+grant select on dbamv.convenio              to upflux;
+grant select on dbamv.ent_pro               to upflux;
+grant select on dbamv.especie               to upflux;
+grant select on dbamv.estoque               to upflux;
+grant select on dbamv.gru_cid               to upflux;
+grant select on dbamv.gru_fat               to upflux;
+grant select on dbamv.gru_pro               to upflux;
+grant select on dbamv.grupo_cirurgia        to upflux;
+grant select on dbamv.guia                  to upflux;
+grant select on dbamv.hr_sal_cir 			to upflux;
+grant select on dbamv.it_guia               to upflux;
+grant select on dbamv.itent_pro             to upflux;
+grant select on dbamv.itmvto_estoque        to upflux;
+grant select on dbamv.itreg_fat             to upflux;
+grant select on dbamv.log_guia              to upflux;
+grant select on dbamv.log_mvto_consignado   to upflux;
+grant select on dbamv.mot_canc              to upflux;
+grant select on dbamv.mov_cc_rpa            to upflux;
+grant select on dbamv.multi_empresas        to upflux;
+grant select on dbamv.mvto_estoque          to upflux;
+grant select on dbamv.ori_ate               to upflux;
+grant select on dbamv.paciente              to upflux;
+grant select on dbamv.prestador             to upflux;
+grant select on dbamv.prestador_aviso       to upflux;
+grant select on dbamv.pro_fat               to upflux;
+grant select on dbamv.produto               to upflux;
+grant select on dbamv.reg_fat               to upflux;
+grant select on dbamv.sal_cir               to upflux;
+grant select on dbamv.servico               to upflux;
+grant select on dbamv.setor                 to upflux;
+grant select on dbamv.sgru_cid              to upflux;
+grant select on dbamv.sub_grupo_cirurgia    to upflux;
+grant select on dbamv.tip_anest             to upflux;
+grant select on dbamv.tuss                  to upflux;
+grant select on dbamv.uni_pro               to upflux;
+grant select on dbasgu.usuarios 			to upflux;
+
+
+GRANT SELECT ON DBAMV.TIP_RES                 TO upflux;
+GRANT SELECT ON DBAMV.SACR_TEMPO_PROCESSO                 TO upflux; -- TABELA NÃO EXISTE
+GRANT SELECT ON DBAMV.TRIAGEM_ATENDIMENTO                 TO upflux;
+GRANT SELECT ON DBAMV.sacr_tipo_tempo_processo            TO upflux;
+GRANT SELECT ON DBAMV.FILA_SENHA            TO upflux;
